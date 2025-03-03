845ece13e13f537a2eda4458cfb408f4fad6565c
MarchanoGG <56337567+MarchanoGG@users.noreply.github.com>
2025-03-03 02:26:01 +0100

    Books Array sorted on title ASC

--- js/books.js
+++ js/books.js
@@ -1,62 +1,26 @@
 const books = [
     {
-        title: "The Flight of the Eisenstein",
-        author: "James Swallow",
-        image: "the_flight_of_eisenstein.jpg",
-        rating: 1
-    },
-    {
-        title: "Galaxy in Flames",
-        author: "Ben Counter",
-        image: "galaxe_in_flames.jpg",
-        rating: 3
-    },
-    {
-        title: "False Gods",
-        author: "Graham McNeill",
-        image: "false_gods.jpg",
-        rating: 3
-    },
-    {
-        title: "Horus Rising",
-        author: "Dan Abnett",
-        image: "horus_rising.jpg",
-        rating: 5
-    },
-    {
-        title: "Project Hail Mary",
-        author: "Andy Weir",
-        image: "project_hail_mary.jpg",
-        rating: 5
-    },
-    {
-        title: "The Redemption of Time",
-        author: "Baoshu",
-        image: "the_redemption_of_time.jpg",
+        title: "Broken Stars",
+        author: "Ken Liu",
+        image: "broken_stars.jpg",
         rating: 2
     },
     {
-        title: "Death's End",
-        author: "Cixin Liu",
-        image: "deaths_end.jpg",
-        rating: 4
-    },
-    {
-        title: "The Dark Forest",
-        author: "Cixin Liu",
-        image: "the_dark_forest.jpg",
-        rating: 1
-    },
-    {
-        title: "The Three-Body Problem",
-        author: "Cixin Liu",
-        image: "three_body_problem.jpg",
-        rating: 1
+        title: "Children of Dune",
+        author: "Frank Herbert",
+        image: "dune_3.jpg",
+        rating: 5
     },
     {
         title: "Children of Time",
         author: "Adrian Tchaikovsky",
         image: "children_of_time.jpg",
+        rating: 4
+    },
+    {
+        title: "Death's End",
+        author: "Cixin Liu",
+        image: "deaths_end.jpg",
         rating: 4
     },
     {
@@ -72,10 +36,46 @@ const books = [
         rating: 4
     },
     {
-        title: "Children of Dune",
-        author: "Frank Herbert",
-        image: "dune_3.jpg",
+        title: "False Gods",
+        author: "Graham McNeill",
+        image: "false_gods.jpg",
+        rating: 3
+    },
+    {
+        title: "Galaxy in Flames",
+        author: "Ben Counter",
+        image: "galaxe_in_flames.jpg",
+        rating: 3
+    },
+    {
+        title: "Horus Rising",
+        author: "Dan Abnett",
+        image: "horus_rising.jpg",
         rating: 5
+    },
+    {
+        title: "Project Hail Mary",
+        author: "Andy Weir",
+        image: "project_hail_mary.jpg",
+        rating: 5
+    },
+    {
+        title: "The Dark Forest",
+        author: "Cixin Liu",
+        image: "the_dark_forest.jpg",
+        rating: 1
+    },
+    {
+        title: "The Flight of the Eisenstein",
+        author: "James Swallow",
+        image: "the_flight_of_eisenstein.jpg",
+        rating: 1
+    },
+    {
+        title: "The Redemption of Time",
+        author: "Baoshu",
+        image: "the_redemption_of_time.jpg",
+        rating: 2
     },
     {
         title: "The Supernova Era",
@@ -84,11 +84,9 @@ const books = [
         rating: 2
     },
     {
-        title: "Broken Stars",
-        author: "Ken Liu",
-        image: "broken_stars.jpg",
-        rating: 2
+        title: "The Three-Body Problem",
+        author: "Cixin Liu",
+        image: "three_body_problem.jpg",
+        rating: 1
     }
 ];
-
-console.log(books);
