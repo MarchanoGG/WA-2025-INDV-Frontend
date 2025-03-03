560b370516db8b4865732dfdc0891b4419027a4b
MarchanoGG <56337567+MarchanoGG@users.noreply.github.com>
2025-03-03 02:45:03 +0100

    Realistic book ratings

--- js/books.js
+++ js/books.js
@@ -3,90 +3,90 @@ const books = [
         title: "Broken Stars",
         author: "Ken Liu",
         image: "broken_stars.jpg",
-        rating: 2
+        rating: 4.5
     },
     {
         title: "Children of Dune",
         author: "Frank Herbert",
         image: "dune_3.jpg",
-        rating: 5
+        rating: 3.5
     },
     {
         title: "Children of Time",
         author: "Adrian Tchaikovsky",
         image: "children_of_time.jpg",
-        rating: 4
+        rating: 1
     },
     {
         title: "Death's End",
         author: "Cixin Liu",
         image: "deaths_end.jpg",
-        rating: 4
+        rating: 5
     },
     {
         title: "Dune",
         author: "Frank Herbert",
         image: "dune_1.jpg",
-        rating: 3
+        rating: 4
     },
     {
         title: "Dune Messiah",
         author: "Frank Herbert",
         image: "dune_2.jpg",
-        rating: 4
+        rating: 3.5
     },
     {
         title: "False Gods",
         author: "Graham McNeill",
         image: "false_gods.jpg",
-        rating: 3
+        rating: 4
     },
     {
         title: "Galaxy in Flames",
         author: "Ben Counter",
         image: "galaxe_in_flames.jpg",
-        rating: 3
+        rating: 3.5
     },
     {
         title: "Horus Rising",
         author: "Dan Abnett",
         image: "horus_rising.jpg",
-        rating: 5
+        rating: 4.5
     },
     {
         title: "Project Hail Mary",
         author: "Andy Weir",
         image: "project_hail_mary.jpg",
-        rating: 5
+        rating: 3
     },
     {
         title: "The Dark Forest",
         author: "Cixin Liu",
         image: "the_dark_forest.jpg",
-        rating: 1
+        rating: 5
     },
     {
         title: "The Flight of the Eisenstein",
         author: "James Swallow",
         image: "the_flight_of_eisenstein.jpg",
-        rating: 1
+        rating: 2
     },
     {
         title: "The Redemption of Time",
         author: "Baoshu",
         image: "the_redemption_of_time.jpg",
-        rating: 2
+        rating: 4.5
     },
     {
         title: "The Supernova Era",
         author: "Cixin Liu",
         image: "the_supernova_era.jpg",
-        rating: 2
+        rating: 3
     },
     {
         title: "The Three-Body Problem",
         author: "Cixin Liu",
         image: "three_body_problem.jpg",
-        rating: 1
+        rating: 4.5
     }
 ];
